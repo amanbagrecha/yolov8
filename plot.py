4cfe1336be9460348a27a71247806b80c39beb95
Aman Bagrecha <bagrecha.aman588@gmail.com>
2023-04-22 22:04:37 +0530

    plot function

---
+++ plot.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from PIL import Image
+from matplotlib import patches
+
+from datasets import load_dataset
+
+
+data = load_dataset("Francesco/solar-panels-taxvb")
+split_name = 'train'
+index_value = 9
+img_no = data[split_name]['image_id'].index(index_value)
+
+_, _, bboxs, labels = data[split_name]["objects"][img_no].values()
+image_id = data[split_name]["image_id"][img_no]
+
+with Image.open(f'datasets/images/{split_name}/{image_id}.png') as f:
+
+    fig, ax = plt.subplots()
+
+    # Display the image
+    ax.imshow(f)
+
+    for idx, (bbox, label) in enumerate(zip(bboxs,labels)):
+        print(f"{label=} and {bbox=}")
+        ax.add_patch(patches.Rectangle((bbox[0], bbox[1]),bbox[2],bbox[3], fill=False, edgecolor='blue', lw=3))
+        ax.text(bbox[0]+10,(bbox[1]+10),str(label),verticalalignment='top',color='white',fontsize=10,weight='bold')
+
+    plt.show()
--- prepare.py
+++ prepare.py
@@ -6,16 +6,18 @@ from datasets import load_dataset
 data = load_dataset("Francesco/solar-panels-taxvb")
 
 
-os.makedirs("labels/train/", exist_ok=True)
-os.makedirs("labels/val/", exist_ok=True)
-os.makedirs("images/train/", exist_ok=True)
-os.makedirs("images/val/", exist_ok=True)
+os.makedirs("datasets/labels/train/", exist_ok=True)
+os.makedirs("datasets/labels/val/", exist_ok=True)
+os.makedirs("datasets/images/train/", exist_ok=True)
+os.makedirs("datasets/images/val/", exist_ok=True)
+os.makedirs('datasets/labels/test/', exist_ok=True)
+os.makedirs('datasets/images/test/', exist_ok=True)
 
 for train_dict in data["train"]:
 
-    train_dict["image"].save(f"images/train/{train_dict['image_id']}.png")
+    train_dict["image"].save(f"datasets/images/train/{train_dict['image_id']}.png")
 
-    with open(f"labels/train/{train_dict['image_id']}.txt", "w") as f:
+    with open(f"datasets/labels/train/{train_dict['image_id']}.txt", "w") as f:
         _, _, bboxs, labels = train_dict["objects"].values()
         for bbox, label in zip(bboxs, labels):
             print(bbox, label)
@@ -27,11 +29,28 @@ for train_dict in data["train"]:
             height = bbox[3] / img_height
             f.write(f"{label} {x_center} {y_center} {width} {height}\n")
 
+
 for train_dict in data["validation"]:
 
-    train_dict["image"].save(f"images/val/{train_dict['image_id']}.png")
+    train_dict["image"].save(f"datasets/images/val/{train_dict['image_id']}.png")
 
-    with open(f"labels/val/{train_dict['image_id']}.txt", "w") as f:
+    with open(f"datasets/labels/val/{train_dict['image_id']}.txt", "w") as f:
+        _, _, bboxs, labels = train_dict["objects"].values()
+        for bbox, label in zip(bboxs, labels):
+            print(bbox, label)
+            img_width = train_dict["width"]
+            img_height = train_dict["height"]
+            x_center = (bbox[0] + (bbox[2]) / 2) / img_width
+            y_center = (bbox[1] + (bbox[3]) / 2) / img_height
+            width = bbox[2] / img_width
+            height = bbox[3] / img_height
+            f.write(f"{label} {x_center} {y_center} {width} {height}\n")
+
+for train_dict in data["test"]:
+
+    train_dict["image"].save(f"datasets/images/test/{train_dict['image_id']}.png")
+
+    with open(f"datasets/labels/test/{train_dict['image_id']}.txt", "w") as f:
         _, _, bboxs, labels = train_dict["objects"].values()
         for bbox, label in zip(bboxs, labels):
             print(bbox, label)
